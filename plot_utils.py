e5dc4cce3adfaa3466679c7c27db541206807c8f
arushiv <arushiv@umich.edu>
2018-08-29 10:04:23 -0400

    defaults for main x and y labels

--- plot_utils.py
+++ plot_utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 from faceted_jointplots import SeabornFig2Grid 
-
+plt.rcParams.update({'figure.max_open_warning': 0})
 
 
 def myjoint(group, x, y, **kwargs):
@@ -18,6 +18,9 @@ def myjoint(group, x, y, **kwargs):
     cmap = kwargs.get('cmap', "Blues")
     x_range = kwargs.get('x_range', None)
     y_range = kwargs.get('y_range', None)
+    main_x_label = kwargs.get('main_x_label', "")
+    main_y_label = kwargs.get('main_y_label', "")
+    
     N = len(group.index)
 
     g = sns.jointplot(data=group, x=x, y=y, kind=kind, joint_kws={'gridsize':gridsize, 'mincnt':0, 'cmap':cmap, 'bins':N}, xlim=x_range, ylim=y_range)
@@ -34,26 +37,33 @@ def myjoint(group, x, y, **kwargs):
 
     # Label Row and y axis on leftmost facets
     if group.name[1] == kwargs['ylabcol']:
-        g.ax_joint.set_ylabel(f"{group.name[0]}\n\n{kwargs['main_y_label']}")
+        g.ax_joint.set_ylabel(f"{group.name[0]}\n\n{main_y_label}")
 
     # Label x axis on lowermost facets
     if group.name[0] == kwargs['xlabcol']:
-        g.ax_joint.set_xlabel(kwargs['main_x_label'])
+        g.ax_joint.set_xlabel(main_x_label)
 
-    # # Label rows on the rightmost facets
-    # if group.name[1] == kwargs['rowLabel']:
-    #     g.ax_marg_y.set_title
-                                                 
     return g
 
 
 def makeGrid(d, row, col, x, y, plotfunc=myjoint, figsize=(6,6), figname=None, **kwargs):
-    """Take pandas dataframe and make a grid of jointplots for variables x and y, faceted over given 'row' and 'column' columns of the dataframe.
-    Usage: makeGrid(d, 'rowCol', 'colCol', 'x', 'y', {'kind':'kde'})
+    """Take a pandas dataframe and make a grid of jointplots for variables x and y, faceted over given 'row' and 'column' columns of the dataframe.
+    Usage : makeGrid(d, 'rowCol', 'colCol', 'x', 'y', {'kind':'kde'})
+    row : dataframe column name of categorical variables which would make rows
+    col : dataframe column name of categorical variables which would make columns
+    x : dataframe column name for x axis
+    y : dataframe column name for y axis
+    plotfunc : function to plot. Default = myjoint which makes a jointplot optionally taking in kwargs
+    figsize : (width, height) in inches
     kwargs: 
-    gridsize = Jointplot hexbin gridsize
-    main_x_label = x label on the lowermost facets
-    main_y_label = y label on the leftmost facets
+    kind : Kind of seaborn jointplot
+    gridsize : Jointplot hexbin gridsize
+    x_label : Default True. Will label x axis for each facet. 
+    y_label : Default True. Will label y axis for each facet. 
+    main_x_label : x label on the lowermost facets
+    main_y_label : y label on the leftmost facets
+    x_range : range of x axis (tuple)
+    y_range : range of y axis (tuple)
     """
     
     nrow = len(d[row].drop_duplicates())
@@ -78,5 +88,4 @@ def makeGrid(d, row, col, x, y, plotfunc=myjoint, figsize=(6,6), figname=None, *
     gs.tight_layout(fig)
     
     plt.savefig(figname)
-    
-    #plt.show()
+
